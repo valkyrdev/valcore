fb56ec96e8533b9a514706689a4834a8c4e88cd1
quindao <39583189+quindao@users.noreply.github.com>
2026-08-02 21:22:58 -0400

    fix mod id

--- src/main/java/com/valkyrdev/valcore/valcore.java
+++ src/main/java/com/valkyrdev/valcore/valcore.java
@@ -10,8 +10,8 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
  @Mod(modid = "com/valkyrdev", name = "Valcore", version = "1.0.0")
  public class valcore {
      public static final String NAME = "Valcore";
-     public static final String MODID = "com/valkyrdev";
-     public static final String VERSION = "1.0.0";
+     public static final String MODID = "valcore";
+     public static final String VERSION = "1.1.0";
 
      @Mod.Instance("com/valkyrdev")
      public static valcore instance;
